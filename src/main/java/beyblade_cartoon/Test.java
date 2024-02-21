8b41cfcf26a441bc3451cba027f6f0f5a64e69b9
duyguyilmaz1 <duy.yilmz@gmail.com>
2024-02-21 15:21:32 +0300

    reload

--- src/main/java/beyblade_cartoon/Test.java
+++ src/main/java/beyblade_cartoon/Test.java
@@ -1,7 +1,34 @@
 package beyblade_cartoon;
 
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        //polymorphism örneği:
+
+        System.out.println("Beyblade programına hoşgeldiniz...");
+        System.out.println("Çıkış için q'ya basınız");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true){
+            System.out.println("Hangi beyblade'i üretmek istiyorsunuz?");
+            String islem= scanner.nextLine();
+            if (islem.equals("q")){
+                System.out.println("programdan çıkılıyor...");
+                break;
+            }else {
+                BeybladeFabrikasi fabrika= new BeybladeFabrikasi();
+                Beyblade beyblade= fabrika.beybladeUret(islem);
+                if (beyblade==null){
+                    System.out.println("Lütfen geçerli bir beyblade ismi giriniz..");
+                }else {
+                    beyblade.bilgileriGoster();
+                    beyblade.saldir();
+                    beyblade.kutsalCanavarOrtayaCikar();
+                }
+            }
+        }
 
 
     }
